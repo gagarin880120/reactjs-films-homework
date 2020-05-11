1ead862efa1e629ebb0e0c1b3f54001a1baf2393
Andrei M <gagarin880120@gmail.com>
2020-05-11 19:22:31 +0300

    feat: add WatchNowButtonRect component

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchField from '../../components/SearchField/SearchField';
 import MovieInfo from '../../components/MovieInfo/MovieInfo';
 import ViewInfoButton from '../../components/ViewInfoButton/ViewInfoButton';
+import WatchNowButtonRect from '../../components/WatchNowButtonRect/WatchNowButtonRect';
 import styles from './MovieDetailsPage.module.scss';
 
 export default function MovieDetailsPage() {
@@ -18,6 +19,7 @@ export default function MovieDetailsPage() {
       <main className={styles.main}>
         <MovieInfo />
         <div className={styles.buttonsContainer}>
+          <WatchNowButtonRect />
           <ViewInfoButton />
         </div>
       </main>
