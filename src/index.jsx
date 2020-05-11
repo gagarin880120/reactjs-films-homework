8da8264491aa5565a202195f6586c0de4674e5bc
Andrei M <gagarin880120@gmail.com>
2020-05-11 13:50:30 +0300

    feat: create App component and add it to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Signature from './components/Signature/Signature';
+import App from './App';
 
 ReactDOM.render(
-  <Signature name="Andrei Mandryk" />,
+  <App />,
   document.getElementById('root'),
 );
