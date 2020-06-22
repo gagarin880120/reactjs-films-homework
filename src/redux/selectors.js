f6802b9935b5fae653a62f9daa0491110f557f84
Andrei M <gagarin880120@gmail.com>
2020-06-22 12:14:30 +0300

    fix: some minor changes

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,8 +2,8 @@ import getGenresString from '../helpers/helpers';
 
 const searchResultsSelector = (state) => state.searchResults.map((v) => ({
     ...v,
-    genres = getGenresString(v.genre_ids, state.genres),
-  }));
+    genres: getGenresString(v.genre_ids, state.genres),
+}));
 
 const modalSelector = (state) => state.isModalOpen;
 
