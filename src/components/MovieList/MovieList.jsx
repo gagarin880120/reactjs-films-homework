453856181000abf1e5b45af745a999722190dfe7
Andrei M <gagarin880120@gmail.com>
2020-06-26 18:12:00 +0300

    fix: change some styles

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -6,22 +6,21 @@ import TrailerModalContainer from '../TrailerModal';
 import MovieListNavBarContainer from '../MovieListNavBar';
 
 export default function MovieList({ results, isModalOpen }) {
-
   return (
-    <>
+    <div className={styles.wrapper}>
       <MovieListNavBarContainer />
-      <div className={styles.wrapper}>
+      <div className={styles.container}>
         {results.map((result) => (
           <MovieCardContainer
             key={result.id}
             movie={result}
           />
         ))}
-        {
-          isModalOpen ? <TrailerModalContainer /> : null
-        }
       </div>
-    </>
+      {
+        isModalOpen ? <TrailerModalContainer /> : null
+      }
+    </div>
   );
 }
 
