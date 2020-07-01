44eb956e1dee8c1b9d4e3a200ad7feb14036f385
Andrei M <gagarin880120@gmail.com>
2020-07-01 11:51:03 +0300

    fix: some minor changes

--- src/components/MovieCard/MovieCardContainer.jsx
+++ src/components/MovieCard/MovieCardContainer.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import MovieCard from './MovieCard';
-import { setModal, getTrailer, getMovieDetails } from '../../redux/actions';
+import {
+  setModal, getTrailer, getMovieDetails,
+} from '../../redux/actions';
 
 export function MovieCardContainer({ onTrailerButtonClick, movie, onLinkClick }) {
   return (
@@ -20,8 +22,8 @@ export const mapDispatchToProps = (dispatch) => ({
     dispatch(getTrailer(id));
   },
   onLinkClick(id) {
-    dispatch(getMovieDetails(id))
-  }
+    dispatch(getMovieDetails(id));
+  },
 });
 
 MovieCardContainer.propTypes = {
