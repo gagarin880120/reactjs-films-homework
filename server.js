f0484099e1099aed7c2cde102d6950ed607f6a9d
Andrei M <gagarin880120@gmail.com>
2020-07-03 14:43:34 +0300

    fix: change server.js to work with react-router in development mode

--- server.js
+++ server.js
@@ -12,14 +12,25 @@ const config = require('./config/webpack.dev.js');
 const compiler = webpack(config);
 
 if (process.env.NODE_ENV === 'development') {
-  // Tell express to use the webpack-dev-middleware and use the webpack.config.js
-  // configuration file as a base.
   app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
   }));
+
   app.use(webpackHotMiddleware(compiler, {
     log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
   }));
+
+  app.use('*', (req, res, next) => {
+    const filename = path.resolve(compiler.outputPath, 'index.html');
+    compiler.outputFileSystem.readFile(filename, (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      res.set('content-type', 'text/html');
+      res.send(result);
+      res.end();
+    });
+  });
 } else {
   app.use(express.static(DIST_DIR));
   app.get('*', (req, res) => res.sendFile(HTML_FILE));
