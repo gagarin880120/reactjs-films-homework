e0b94054a931d73f8d3ca2c167ba783722375f91
Andrei M <gagarin880120@gmail.com>
2020-07-05 12:47:48 +0300

    feat: add link to main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SearchFieldContainer from '../SearchField';
 import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>
-        Films
-      </h1>
+      <Link to="/">
+        <h1 className={styles.title}>
+          Films
+        </h1>
+      </Link>
       <SearchFieldContainer />
     </header>
   );
